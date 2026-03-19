6bf6d44243f729af194d69f2db26dce0c78de290
maistro33 <maistro4733@gmail.com>
2026-03-19 12:38:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,12 @@ TP1_RATIO = 0.50
 
 MIN_VOLUME = 2000000
 MAX_SPREAD = 0.003
-SCAN_DELAY = 10
+SCAN_DELAY = 12
 
 TIMEOUT = 21600
 SL_PCT = 0.03
 
-API_DELAY = 0.25
+API_DELAY = 0.35
 
 bot = telebot.TeleBot(os.getenv("TELE_TOKEN"))
 CHAT_ID = os.getenv("MY_CHAT_ID")
@@ -37,7 +37,7 @@ exchange = ccxt.bitget({
 })
 
 markets = exchange.load_markets()
-SYMBOLS = [s for s in markets if markets[s]["swap"] and "USDT" in s][:200]
+SYMBOLS = [s for s in markets if markets[s]["swap"] and "USDT" in s][:150]
 
 trade_state = {}
 cooldown = {}
